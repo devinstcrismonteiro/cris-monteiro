41f698eae3e062056acd034c8c43d57fb977b19a
eduhdev <eduardocarvalho970@gmail.com>
2023-08-22 17:31:41 -0300

    Temporarily no courses or posts | Fix deploy issues

--- src/pages/curso/[slug].js
+++ src/pages/curso/[slug].js
@@ -3,44 +3,45 @@ import Course from '../../templates/Course'
 import { getAllCourses, getSingleCourse } from '../../graphql/functions'
 
 export default function PageCourse(props) {
-  return <Course {...props} />
+  return <h1>Single Course Page</h1>
+  //  <Course {...props} />
 }
 
-export async function getStaticProps({ params = {} } = {}) {
-  const course = await getSingleCourse(params.slug)
-  const { course_details } = course
-  const { description } = course_details
+// export async function getStaticProps({ params = {} } = {}) {
+//   const course = await getSingleCourse(params.slug)
+//   const { course_details } = course
+//   const { description } = course_details
 
-  const data = {
-    ...course,
-    course_details: {
-      ...course_details,
-      description: description?.replace(/(?:\r\n|\r|\n)/g, '<br>'),
-    },
-  }
+//   const data = {
+//     ...course,
+//     course_details: {
+//       ...course_details,
+//       description: description?.replace(/(?:\r\n|\r|\n)/g, '<br>'),
+//     },
+//   }
 
-  return {
-    props: {
-      revalidate: 10,
-      data
-    },
-  }
-}
+//   return {
+//     props: {
+//       revalidate: 10,
+//       data
+//     },
+//   }
+// }
 
-export async function getStaticPaths() {
-  const courses = await getAllCourses()
+// export async function getStaticPaths() {
+//   const courses = await getAllCourses()
 
-  const paths = courses.map((course) => {
-    const { slug } = course
-    return {
-      params: {
-        slug,
-      },
-    }
-  })
+//   const paths = courses.map((course) => {
+//     const { slug } = course
+//     return {
+//       params: {
+//         slug,
+//       },
+//     }
+//   })
 
-  return {
-    paths,
-    fallback: false,
-  }
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   }
+// }
